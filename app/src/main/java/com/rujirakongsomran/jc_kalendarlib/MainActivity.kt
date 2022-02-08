cf1c2e5f5300f12ee3238246912d497155154136
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-02-08 12:23:43 +0700

    handle OnCurrentDayClick

--- app/src/main/java/com/rujirakongsomran/jc_kalendarlib/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_kalendarlib/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.rujirakongsomran.jc_kalendarlib
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.himanshoe.kalendar.common.KalendarSelector
 import com.himanshoe.kalendar.common.KalendarStyle
@@ -36,6 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateCalendar() {
+    val context = LocalContext.current
     Kalendar(
         kalendarType = KalendarType.Firey(),
         kalendarStyle = KalendarStyle(
@@ -43,12 +47,18 @@ fun CreateCalendar() {
         ),
         kalendarEvents = listOf(
             KalendarEvent(
-                date = LocalDate.of(2022, 1, 28),
-                eventName = "HBD Biw",
-                eventDescription = "biwberry was born."
+                date = LocalDate.of(2022, 2, 28),
+                eventName = "Biw Day",
+                eventDescription = "Celebrate"
             )
         ),
         onCurrentDayClick = { day, event ->
+            Toast.makeText(
+                context,
+                "${event?.eventName}",
+                Toast.LENGTH_SHORT
+            ).show()
+
         }, errorMessage = {
 
         })
