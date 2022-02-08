b11d7472e174a0e0460ef98a7d44ab5670806c11
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-02-08 12:17:18 +0700

    KalendarEvent

--- app/src/main/java/com/rujirakongsomran/jc_kalendarlib/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_kalendarlib/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.himanshoe.kalendar.common.KalendarSelector
 import com.himanshoe.kalendar.common.KalendarStyle
+import com.himanshoe.kalendar.common.data.KalendarEvent
 import com.himanshoe.kalendar.ui.Kalendar
 import com.himanshoe.kalendar.ui.KalendarType
 import com.rujirakongsomran.jc_kalendarlib.ui.theme.JC_KalendarLibTheme
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +40,13 @@ fun CreateCalendar() {
         kalendarType = KalendarType.Firey(),
         kalendarStyle = KalendarStyle(
             kalendarSelector = KalendarSelector.Circle(),
+        ),
+        kalendarEvents = listOf(
+            KalendarEvent(
+                date = LocalDate.of(2022, 1, 28),
+                eventName = "HBD Biw",
+                eventDescription = "biwberry was born."
+            )
         ),
         onCurrentDayClick = { day, event ->
         }, errorMessage = {
